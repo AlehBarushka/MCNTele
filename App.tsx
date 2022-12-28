ac6142acc345ef7d241ddf7709e4e484e1129ead
Aleh Barushka <1.alehbarushka@gmail.com>
2022-12-28 13:39:30 +0300

    feat: :heavy_plus_sign: add safe area provider

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 import { store } from "./src/redux/store";
 
 const App = () => {
   return (
     <Provider store={store}>
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <SafeAreaProvider>
+        <StatusBar style="auto" />
+        <SafeAreaView style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </SafeAreaView>
+      </SafeAreaProvider>
     </Provider>
   );
-}
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
   container: {
