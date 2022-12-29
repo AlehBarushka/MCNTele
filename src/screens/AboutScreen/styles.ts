a70007fdd8859b35728cddb73deddae1e67a4ad6
Aleh Barushka <1.alehbarushka@gmail.com>
2022-12-29 16:38:25 +0300

    feat: :sparkles: add about screen

---
+++ src/screens/AboutScreen/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  text: {
+    marginBottom: 8,
+    fontSize: 18,
+    textAlign: "center",
+  },
+  textBold: {
+    fontWeight: "600",
+  },
+});
+
+export default styles;
